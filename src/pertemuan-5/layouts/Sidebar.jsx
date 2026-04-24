252c8d3b51f0152eb1d75e11f9c93f7904eff898
Nabilasurya <surya24si@mahasiswa.pcr.ac.id>
2026-04-24 16:51:37 +0700

    p5-Belajar Mandiri

---
+++ src/pertemuan-5/layouts/Sidebar.jsx
@@ -0,0 +1,116 @@
+import { FaHome, FaShoppingCart, FaUsers, FaList, FaPlus } from "react-icons/fa";
+
+export default function Sidebar() {
+    return (
+        <div
+            id="sidebar"
+            className="flex min-h-screen w-[360px] flex-col bg-white p-10 shadow-lg"
+        >
+            {/* Logo */}
+            <div id="sidebar-logo" className="flex flex-col">
+                <span
+                    id="logo-title"
+                    className="font-poppins text-[48px] text-gray-900 font-bold"
+                >
+                    Sedap <b id="logo-dot" className="text-hijau">.</b>
+                </span>
+                <span
+                    id="logo-subtitle"
+                    className="font-semibold text-gray-400"
+                >
+                    Modern Admin Dashboard
+                </span>
+            </div>
+
+            {/* Menu */}
+            <div id="sidebar-menu" className="mt-10 flex-1">
+                <ul id="menu-list" className="space-y-3">
+                    <li>
+                        <div
+                            id="menu-1"
+                            className="hover:text-hijau flex cursor-pointer items-center rounded-xl p-4 font-medium text-gray-600 hover:bg-green-200 hover:font-extrabold"
+                        >
+                            <FaHome className="mr-4 text-xl" />
+                            Dashboard
+                        </div>
+                    </li>
+
+                    <li>
+                        <div
+                            id="menu-2"
+                            className="hover:text-hijau flex cursor-pointer items-center rounded-xl p-4 font-medium text-gray-600 hover:bg-green-200 hover:font-extrabold"
+                        >
+                            <FaShoppingCart className="mr-4 text-xl" />
+                            Orders
+                        </div>
+                    </li>
+
+                    <li>
+                        <div
+                            id="menu-3"
+                            className="hover:text-hijau flex cursor-pointer items-center rounded-xl p-4 font-medium text-gray-600 hover:bg-green-200 hover:font-extrabold"
+                        >
+                            <FaUsers className="mr-4 text-xl" />
+                            Customers
+                        </div>
+                    </li>
+
+                    <li>
+                        <div
+                            id="menu-4"
+                            className="hover:text-hijau flex cursor-pointer items-center rounded-xl p-4 font-medium text-gray-600 hover:bg-green-200 hover:font-extrabold"
+                        >
+                            <FaList className="mr-4 text-xl" />
+                            Menu List
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            {/* Footer */}
+            <div id="sidebar-footer" className="mt-auto">
+                <div
+                    id="footer-card"
+                    className="bg-hijau px-4 py-2 rounded-md shadow-lg mb-10 flex items-center justify-between"
+                >
+                    <div id="footer-text" className="text-white text-sm">
+                        <span>
+                            Please organize your menus through button below!
+                        </span>
+
+                        <div
+                            id="add-menu-button"
+                            className="flex justify-center items-center p-2 mt-3 bg-white rounded-md space-x-2"
+                        >
+                            <FaPlus className="text-gray-600" />
+                            <span className="text-gray-600 flex items-center">
+                                Add Menus
+                            </span>
+                        </div>
+                    </div>
+
+                    <img
+                        id="footer-avatar"
+                        className="w-20 rounded-full"
+                        src="https://avatar.iran.liara.run/public/28"
+                        alt="avatar"
+                    />
+                </div>
+
+                <span
+                    id="footer-brand"
+                    className="font-bold text-gray-400"
+                >
+                    Sedap Restaurant Admin Dashboard
+                </span>
+
+                <p
+                    id="footer-copyright"
+                    className="font-light text-gray-400"
+                >
+                    &copy; 2025 All Right Reserved
+                </p>
+            </div>
+        </div>
+    );
+}
